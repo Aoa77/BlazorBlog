d6e097fd82208407ce6875d971e6fa35ffe9be7d
aoa77 <alalbers77@gmail.com>
2024-02-24 03:02:00 -0600

    qp

--- src/BlazorBlog.Wasm/engine/DeviceWidth.cs
+++ src/BlazorBlog.Wasm/engine/DeviceWidth.cs
@@ -2,7 +2,7 @@
 
 public static class DeviceWidth
 {
-    public const int Bumper = 160;
+    public const int Bumper = 260;
     public const double iPhoneSE = 320 + Bumper;
     public const double iPhone678 = 375 + Bumper;
 }
